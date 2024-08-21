28e912624a0a7130ef129224ba1632b25b8a8ae8
Donnie Ellis <donnie@dmellis.com>
2024-08-20 21:34:40 -0400

    disable cache on the picks route

--- app/draft/[draftId]/kiosk/page.tsx
+++ app/draft/[draftId]/kiosk/page.tsx
@@ -18,7 +18,6 @@ import useSWR from 'swr';
 type MemoizedDraft = Omit<Draft, 'picks'> & { picks: PickWithPlayerAndTeam[] };
 
 const fetcher = (url: string) => fetch(url).then(res => res.json());
-export const fetchCache = 'force-no-store';
 
 const KioskPage: React.FC = () => {
   const params = useParams();
