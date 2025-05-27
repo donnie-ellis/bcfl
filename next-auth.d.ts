4ebd5993126edefe86aeb904bb50986188d80e7f
Donnie Ellis <donnie@dmellis.com>
2025-05-27 14:11:59 -0400

    clean up auth

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,15 +1,12 @@
-import NextAuth from "next-auth"
+import NextAuth, { DefaultSession } from "next-auth";
 
 declare module "next-auth" {
   interface Session {
     accessToken?: string;
     error?: string;
-    refreshToken: string;
+    refreshToken?: string;
     user: {
-      id: string;
-      image?: string;
-      name?: string;
-      email?: string;
-    }
+      id?: string;
+    } & DefaultSession["user"];
   }
 }
