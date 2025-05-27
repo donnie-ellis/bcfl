25f3c3a93d6a5aac8f68a79e22c78baad9f1d9a5
Donnie Ellis <donnie@dmellis.com>
2025-05-27 17:32:08 -0400

    refactored

---
+++ components/LeagueHeaderCard.tsx
@@ -0,0 +1,25 @@
+// ./components/LeagueHeaderCard.tsx
+// This component displays the league header with the logo and name.
+import React from "react";
+import { Card, CardHeader, CardTitle } from "./ui/card";
+import { League } from "@/lib/types";
+
+const LeagueHeaderCard: React.FC<{ league: League | null }> = ({ league }) => {
+    return (
+      league &&
+        <Card>
+                <CardHeader className="flex flex-row items-center space-x-4">
+                  {league.logo_url && (
+                    <img 
+                      src={league.logo_url} 
+                      alt={league.name} 
+                      className="w-16 h-16 rounded"
+                    />
+                  )}
+                  <CardTitle>{league.name}</CardTitle>
+                </CardHeader>
+              </Card>
+    );
+}
+
+export default LeagueHeaderCard;
